a595b92c39f98c4066591e65d31a740cf00142e5
kreattang <1841187753@qq.com>
2018-11-13 16:46:48 +0800

    Update

--- Object_Detection/Video_Test_Detection.py
+++ Object_Detection/Video_Test_Detection.py
@@ -11,8 +11,6 @@ cap=cv2.VideoCapture('20181112182536rec.mp4')
 while(True):
     ret , frame = cap.read()
 
-    #our operation on the frame come here
-
     #display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) &0xFF ==ord('q'):  #按q键退出
