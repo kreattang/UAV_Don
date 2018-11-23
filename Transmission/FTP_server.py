c0ecc143e3c288feb3a1135278e1ff8ce356f91a
kreattang <1841187753@qq.com>
2018-11-23 20:42:23 +0800

    11/23

--- Transmission/FTP_server.py
+++ Transmission/FTP_server.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2018/11/17 20:48
+# @Time    : 2018/11/19 10:25
 # @Author  : blvin.Don
-# @File    : Server.py
+# @File    : FTP_server.py
 
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
@@ -12,18 +12,20 @@ from pyftpdlib.servers import FTPServer
 authorizer = DummyAuthorizer()
 
 #添加用户权限和路径，括号内的参数是(用户名， 密码， 用户目录， 权限)
-authorizer.add_user('user', '12345', './', perm='elradfmw')
+authorizer.add_user('user', '12345', 'I:/UAVCode/image', perm='elradfmw')
 
 #添加匿名用户 只需要路径
-authorizer.add_anonymous(u'I:/UAVCode/image')
+authorizer.add_anonymous('I:/UAVCode/image')
 
 #初始化ftp句柄
 handler = FTPHandler
 handler.authorizer = authorizer
 
-#监听ip 和 端口,因为linux里非root用户无法使用21端口，所以我使用了2121端口
+#添加被动端口范围
+handler.passive_ports = range(2000, 2333)
+
+#监听ip 和 端口
 server = FTPServer(('192.168.31.16', 8090), handler)
 
 #开始服务
 server.serve_forever()
-
